fa6ae129181e7aa06b1c5a5089d5185d26f77a30
Joe McCorison <joemccorison@gmail.com>
2020-12-15 15:40:26 +0000

    updates loader

--- resources/views/livewire/playground.blade.php
+++ resources/views/livewire/playground.blade.php
@@ -32,12 +32,12 @@
                 @error('message') <span class="error">{{ $message }}</span> @enderror
             </div>
             <button class="bg-black text-white rounded p-2 disabled:opacity-50 inline-flex" type="submit">
-                <svg wire:loading.delay class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <svg wire:loading wire:target="submitForm" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                     <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                     <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                 </svg>
-                <span wire:loading.remove>Submit</span>
-                <span wire:loading.delay>Processing</span>
+                <span wire:loading">Submit</span>
+                <span wire:loading wire:target="submitForm" >Processing</span>
             </button>
         </form>
     </div>
